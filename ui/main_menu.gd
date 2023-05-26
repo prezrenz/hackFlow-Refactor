7325d2846eb3ff42f78063dca8919213165827d5
prezrenz <perezrenjie20@gmail.com>
2023-05-26 16:27:33 +0800

    quit button hides if on web platform now

--- ui/main_menu.gd
+++ ui/main_menu.gd
@@ -8,7 +8,8 @@ extends ColorRect
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	if(OS.has_feature("web")):
+		$VBoxContainer/VBoxContainer/Quit.hide()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
